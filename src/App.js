434a668cd22c45a9137201fe945955f0ee74bcc9
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-09-02 17:37:30 -0400

    debugged error in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.scss';
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
   return (
 <>
 <Routes>
-  <Route path="/" element { <Layout /> }/>
+  <Route path="/" element={<Layout /> }/>
 </Routes>
 </>
   )
