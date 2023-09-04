8df20d8e55ff89aa56c0930b10efba27830ec071
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-09-04 11:38:41 -0400

    added about page/routes and content to the about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,20 @@
 import './index.scss';
 
-const About = () => ('Hello about')
+const About = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1> About Me </h1>
+                <p> 
+                I am a junior full-stack developer. I completed my certification with the University of Miami's EDX Bootcamp. I'm ambitious and eager to expand my knowledge of the latest technologies and gain experience with a role in an established tech company, building diverse projects.
+                </p>
+                Growing up on my little island of Jamaica, I have always been a naturally curious and innovative problem solver. I see so many of those traits in my son, which is why I became a developer. My ultimate goal is to share the same passions as my child while providing the best life I can for him. 
+                <p>
+                I am also a business owner. I started an auto parts company called Turboutique Auto Parts. I have always loved being in the automotive field and would be thrilled to implement my developer skills within the same space.
+                </p>
+            </div>
+        </div>
+    )
+}
 
 export default About;
