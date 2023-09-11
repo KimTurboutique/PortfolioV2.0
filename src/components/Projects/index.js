f9216c5741bcb49a491daf26ed3827f8593823dc
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-09-11 18:49:21 -0400

    added image to projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -14,6 +14,7 @@ import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import turboutique from '../../assets/images/turboutique.net.png';
 import wkdayscheduler from '../../assets/images/wkdaysched.png'
+import carpointment from '../../assets/images/carpointment.png';
 
 function Copyright() {
     return (
@@ -31,7 +32,7 @@ function Copyright() {
 const cards = [
     {
         id: 1,
-        image: '',
+        image: 'carpointment',
         heading: 'Carpointment App',
         content: 'Under Development 🚧 👷🏾‍♀️',
         github: 'https://github.com/KimTurboutique/Carpointment_App'
